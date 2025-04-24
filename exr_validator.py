cdb90ca8317f2b94ebe11e7005d32b65ccad0d5d
João Vitor Cardoso <xoaocardoso@gmail.com>
2025-04-24 11:17:12 -0300

    Initial commit

---
+++ exr_validator.py
@@ -0,0 +1,61 @@
+import os
+import OpenEXR
+import math
+
+inputFolderPath = input("Type the path to the folder containing the files...\n")
+filesList = sorted(os.listdir(inputFolderPath))
+
+validFiles = 0
+invalidFiles = []
+suspiciousFiles = []
+filesSize = 0.0
+
+for i in filesList:
+    try:
+        exr = OpenEXR.InputFile(os.path.join(inputFolderPath, i))
+        filesSize += os.path.getsize(os.path.join(inputFolderPath, i))
+    except Exception as e:
+        invalidFiles.append(i)
+        continue
+
+def getReadableAverageSize(size):
+    if size == 0 or not filesList:
+        return "0B"
+    sizeName = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    avgSize = size / len(filesList)
+    i = int(math.floor(math.log(avgSize, 1024)))
+    p = math.pow(1024, i)
+    s = round(avgSize / p, 2)
+    return [s, sizeName[i]]
+
+window = 10
+
+for i in range(len(filesList)):
+    neighborSizes = [
+        os.path.getsize(os.path.join(inputFolderPath, filesList[j]))
+        for j in range(max(0, i - window), min(len(filesList), i + window + 1)) if j != i
+    ]
+    avgNeighbors = sum(neighborSizes) / len(neighborSizes)
+    currentSize = os.path.getsize(os.path.join(inputFolderPath, filesList[i]))
+
+    if abs(currentSize - avgNeighbors) > (avgNeighbors * 0.2):
+        suspiciousFiles.append(filesList[i])
+        suspiciousFiles = list(set(suspiciousFiles) - set(invalidFiles))
+
+
+print("Analizyse complete.")
+print(f"Total files: {len(filesList)}")
+print(f"Valid files: {len(filesList) - len(invalidFiles)}")
+if len(invalidFiles) > 0:
+    print("Invalid files:")
+    for i in invalidFiles:
+        print(f" - {i}")
+if len(suspiciousFiles) > 0:
+    print("Suspicious files:")
+    for i in suspiciousFiles:
+        print(f" - {i}")
+
+if len(suspiciousFiles) and len(invalidFiles) == 0:
+    print("No problems found.")
+print(f"Average size: {getReadableAverageSize(filesSize)[0]} {getReadableAverageSize(filesSize)[1]}")
+
